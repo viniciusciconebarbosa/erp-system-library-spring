eb5fe14ed57ce6208a6d3d8ea2ae185ba167b643
vinicius <viniciuscicone@gmail.com>
2025-04-17 18:05:23 -0300

    adding template project

---
+++ src/main/java/com/biblioteca/erp_biblioteca/ErpBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.erp_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/erp_biblioteca/ErpBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.erp_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
