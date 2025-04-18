11a48d9eccbfefe9a94853d960965f21663e9d97
vinicius <viniciuscicone@gmail.com>
2025-04-18 18:29:39 -0300

    update usuarioService

--- src/main/java/com/biblioteca/erp_biblioteca/service/UsuarioService.java
+++ src/main/java/com/biblioteca/erp_biblioteca/service/UsuarioService.java
@@ -7,6 +7,7 @@ import com.biblioteca.erp_biblioteca.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
