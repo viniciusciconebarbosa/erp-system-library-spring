3449ccc11a64a6cf9cdfb2f49b5f6b0645ba3676
vinicius <viniciuscicone@gmail.com>
2025-04-18 17:56:39 -0300

    update usuario

--- src/main/java/com/biblioteca/erp_biblioteca/model/Usuario.java
+++ src/main/java/com/biblioteca/erp_biblioteca/model/Usuario.java
@@ -2,14 +2,20 @@ package com.biblioteca.erp_biblioteca.model;
 
 import com.biblioteca.erp_biblioteca.enums.Role;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
 
 import java.util.Set;
 import java.util.UUID;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
@@ -18,64 +24,19 @@ public class Usuario {
     private UUID id;
     
     private String nome;
+
     private int idade;
     
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "doador")
     private Set<Livro> livrosDoados;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "usuario")
     private Set<Locacao> locacoes;
-    
-    // Getters
-    public UUID getId() {
-        return id;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-    
-    public int getIdade() {
-        return idade;
-    }
-    
-    public Role getRole() {
-        return role;
-    }
-    
-    public Set<Livro> getLivrosDoados() {
-        return livrosDoados;
-    }
-    
-    public Set<Locacao> getLocacoes() {
-        return locacoes;
-    }
-    
-    // Setters
-    public void setId(UUID id) {
-        this.id = id;
-    }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    
-    public void setRole(Role role) {
-        this.role = role;
-    }
-    
-    public void setLivrosDoados(Set<Livro> livrosDoados) {
-        this.livrosDoados = livrosDoados;
-    }
-    
-    public void setLocacoes(Set<Locacao> locacoes) {
-        this.locacoes = locacoes;
-    }
 }
