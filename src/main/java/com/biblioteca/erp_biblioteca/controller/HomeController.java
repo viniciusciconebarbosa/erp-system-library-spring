fff7acc313e5ead217776f6b7c1f8aa92540735c
vinicius <viniciuscicone@gmail.com>
2025-04-19 15:49:36 -0300

    adding a index

--- src/main/java/com/biblioteca/erp_biblioteca/controller/HomeController.java
+++ src/main/java/com/biblioteca/erp_biblioteca/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "redirect:/swagger-ui.html";
+        return "forward:/index.html";
     }
     
     @GetMapping("/health")
