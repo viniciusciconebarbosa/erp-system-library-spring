0d112a70b8b85cfb636308bef6c0c1e2aeeae7b8
vinicius <viniciuscicone@gmail.com>
2025-04-21 13:40:33 -0300

    updating picture config

--- src/main/java/com/biblioteca/erp_biblioteca/config/StorageConfig.java
+++ src/main/java/com/biblioteca/erp_biblioteca/config/StorageConfig.java
@@ -3,8 +3,11 @@ package com.biblioteca.erp_biblioteca.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Configuration
@@ -17,4 +20,9 @@ public class StorageConfig {
     private List<String> allowedContentTypes;
     private int imageMaxWidth;
     private int imageMaxHeight;
+
+    @Bean
+    public Path rootLocation() {
+        return Paths.get(uploadDir);
+    }
 }
