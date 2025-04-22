aef8c416814f6dc5dcf19081061d6e9d585496b6
vinicius <viniciuscicone@gmail.com>
2025-04-22 15:52:25 -0300

    adding dto login

---
+++ src/main/java/com/biblioteca/erp_biblioteca/dto/LoginDTO.java
@@ -0,0 +1,15 @@
+package com.biblioteca.erp_biblioteca.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+}
